690c0a90508a76f9f034446137d15650d9aeb02e
huanglifeng <lifeng.huang@oumen.com>
2014-10-26 09:07:31 +0800

    remove some unused files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,20 @@ var router = express.Router();
 var modes = require('../src/client-includes.js');
 var buildify = require('buildify');
 
+var APIKEYS = [
+  '13723479439',
+  'ab95e75b62c50f4c147fed4b9f0249cca9f3012c',
+  '5a77c9795c54194cb34f5b94d276b5c389587d72',
+  '614b982b015e088ea2698cca11bc6e0fc1bbf304',
+  '247b3125296b0f42ed497602d1e41ab5acec2795',
+  'ba9d8c5a4a25e9a4099a087d74c81a6df5ae8107',
+  '3412a953b82a215af76de39975da45ed56568bd3',
+  'c04c7cb9eb22f5cfa33a486325b42c590a09d8a8',
+  '9c0a022c0c2579eecca4f0a100bb735c3ad814cb',
+  'f0479e5f8caaf32d5f927e22f8884343e5f05131',
+  'ed9620446c8cd07e39f3334f45d80540796de4a3'
+];
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
@@ -27,7 +41,7 @@ router.get('/client.js', function (req, res) {
 
   var debug = false;
   try {
-    debug = (domain == 'localhost') || url.parse(req.headers.referer).query == 'debuggee';
+    debug = (domain === 'localhost') || url.parse(req.headers.referer).query === 'debuggee';
   } catch (err) {}
 
   //get info from IP
@@ -62,16 +76,23 @@ router.get('/client.js', function (req, res) {
 //            });
 //        }
 
-  if (key !== '13723479439') {
-    debug = false;
+  if (APIKEYS.indexOf(key) < 0) {
+    console.error('Invalid api key: ', key);
+    res.send(404);
+
+  } else {
+    if (key !== '13723479439') {
+      debug = false;
+    }
+
+    if (!debug) {
+      js = js.uglify();
+    }
+
+    res.setHeader('Content-Type', 'text/javascript');
+    res.send(200, js.content);
   }
 
-  if (!debug) {
-    js = js.uglify();
-  }
-
-  res.setHeader('Content-Type', 'text/javascript');
-  res.send(200, js.content);
 });
 
 // add tracker [img] in router
